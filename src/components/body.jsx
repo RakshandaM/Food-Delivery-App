a7a747c8d0bdf50d628a06ee89b9bc7a468fb84a
RakshandaM <rakshandamadan@gmail.com>
2023-12-16 23:15:46 +0530

    added filter

--- src/components/body.jsx
+++ src/components/body.jsx
@@ -5,27 +5,30 @@ import {useState} from "react";
 
 const Body = () => {
 
-
+  const [listOfResturants, setlistOfResturants]=useState(resList)
+  //Body Render first then useEffect()
+  
   return (
-    <div className="body">
-      <div className="filter">
-        
-        <button
-          className="filter-btn"
-          onClick={() => {
-             const topRatedRestaurants = resList.filter(     // topRatedRestaurants used to store the result of filtering the resList
-              (res) => res.data.avgRating > 4
-            );
-            console.log("Filtered restaurants:", topRatedRestaurants);
-          }}
-        >
+      <div className="body">
+        <div className="filter">
+          
+          <button
+            className="filter-btn"
+            onClick={() => {
+               const topRatedRestaurants = listOfResturants.filter(     // topRatedRestaurants use to store the result of filtering the resList
+                (res) => res.data.avgRating > 4
+              );
+              setlistOfResturants(topRatedRestaurants)
+            }}  
+          >
           Top Rated Restaurant
-        </button>
+          </button>
+        
       
-      </div>
+          </div>
       <div className="res-container">
         {
-          resList.map((restaurant) => (         //map() function iterates over each element of resList
+          listOfResturants.map((restaurant) => (         //map() function iterates over each element of resList
             <RestaurantCard key={restaurant.data.id} resData={restaurant} />
           ))
           //Restaurant - It represents a single item.
