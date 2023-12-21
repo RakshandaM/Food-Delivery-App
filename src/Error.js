8b447e126c126d66d8e57a974c7877a79aa338cc
RakshandaM <rakshandamadan@gmail.com>
2023-12-21 21:25:47 +0530

    error handle

--- src/Error.js
+++ src/Error.js
@@ -1,8 +1,18 @@
+import { useRouteError } from "react-router-dom";  //Handle error in React-router
+
+
 const Error = () =>{
+    const err = useRouteError()
+    console.log(err)
     return(
         <div>
             <h1>Opps!!</h1>
             <h2>Something went wrong</h2>
+            {err ? (
+                <pre>{JSON.stringify(err, null, 2)}</pre>
+            ) : (
+                <p>No error message available</p>
+            )}
         </div>
     );
 };
