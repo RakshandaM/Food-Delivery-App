1358f86aad43b12faac60f88c28ce6c87756f2aa
RakshandaM <rakshandamadan@gmail.com>
2023-12-21 16:36:32 +0530

    added router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Header  from './components/Header';
 import Body from './components/body';
 import React from 'react';
+import About from './components/About';
+import { createBrowserRouter , RouterProvider } from 'react-router-dom'
 
 
 function App() {
@@ -11,6 +13,18 @@ function App() {
       <Body/>
     </div>
   );
-}
+};
+
+export const appRouter = createBrowserRouter ([
+  {
+    path : '/' ,
+    element : <App />,
+  },
+  {
+    path : '/about',
+    element : <About/>
+  },
+])
+
 export default App;
 
